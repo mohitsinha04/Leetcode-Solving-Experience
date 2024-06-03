8faf3b42745a42add49f8f9b948bc30c546de122
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-06-03 12:34:23 +0530

    Time: 295 ms (33.33%) | Memory: 125.5 MB (16.67%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,35 @@
+bool cmp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
+class Solution {
+public:
+    
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end(), cmp);
+        
+        int i = 0;
+        int res = 0;
+        if (meetings[0][0] > 1) {
+            res += abs(meetings[0][0] - 1);
+        }
+       
+        for (i = 0; i < meetings.size() - 1; i++) {
+            if (meetings[i+1][0] <= meetings[i][1]) {
+                if (meetings[i][1] > meetings[i+1][1]) meetings[i+1][1] = meetings[i][1];
+            }
+            if (meetings[i][1] < meetings[i+1][0]) {
+                res += meetings[i+1][0] - meetings[i][1] - 1;
+            }
+        }
+        if (meetings[i][1] < days) {
+            res += abs(days - meetings[i][1]);
+        }
+        
+        return res;
+    }
+};
+// [[1, 3], [5,7], [9,10]]
+// 4, 8;
+
+// [1,3] [2, 4];
+
