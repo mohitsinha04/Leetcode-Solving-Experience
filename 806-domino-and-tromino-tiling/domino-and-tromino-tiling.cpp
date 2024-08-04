60ef6bd088cd3de9031994aaa0cdc5dde25babe5
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-08-04 18:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.01%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -8,25 +8,10 @@ public:
         dp[2] = 2;
         dp[3] = 5;
         
-        // if (n <= 3) return dp[n];
-        
         for (int i = 4; i <= n; i++) {
             dp[i] = 2 * dp[i-1] + dp[i-3];
             dp[i] = dp[i] % mod;
         }
         return dp[n];
-    //     int md=1e9;
-    // md+=7;
-    // vector<long long> v(1001,0);
-    // v[1]=1;
-    // v[2]=2;
-    // v[3]=5;
-    // if(N<=3)
-    //     return v[N];
-    // for(int i=4;i<=N;++i){
-    //     v[i]=2*v[i-1]+v[i-3]; 
-    //     v[i]%=md;
-    // }
-    // return v[N];
     }
 };
