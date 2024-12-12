a5cf6731a08f892a5c56a6c211a416133d966d9a
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-12-12 19:18:25 +0530

    Time: 4 ms (36.22%) | Memory: 21.6 MB (25.00%) - LeetSync

--- 370-range-addition/range-addition.cpp
+++ 370-range-addition/range-addition.cpp
@@ -4,8 +4,7 @@ public:
         vector<int> line(length, 0);
         for (auto update : updates) {
             line[update[0]] += update[2];
-            if (update[1] + 1 < length)
-                line[update[1] + 1] -= update[2];
+            if (update[1] + 1 < length) line[update[1] + 1] -= update[2];
         }
         
         for (int i = 1; i < length; i++) {
