df2baea40cd06044e09269190510f18e48acc716
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-12-13 16:27:10 +0530

    Time: 131 ms (29.85%) | Memory: 71.8 MB (13.34%) - LeetSync

---
+++ 2563-split-message-based-on-limit/split-message-based-on-limit.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<string> splitMessage(string message, int limit) {
+        for (int digit = 1; digit < 5; digit++) {
+            vector<string> res = helper(message, limit, digit);
+            if (res.empty()) continue;
+            return res;
+        }
+        return {};
+    }
+    vector<string> helper(string message, int limit, int digit) {
+        int part = 1, ind = 0;
+        vector<string> res;
+        
+        string placeholder = "";
+        for (int i = 0; i < digit; i++) placeholder += "-";
+        
+        while (ind < message.size()) {
+            string suffix = "<" + to_string(part) + "/" + placeholder + ">";
+            part++;
+            
+            string curr_string = "";
+            int char_left = limit - suffix.size();
+            if (char_left <= 0) return {};
+            
+            while (ind < message.size() && char_left > 0) {
+                curr_string += message[ind];
+                ind++;
+                char_left--;
+            }
+            res.push_back(curr_string);
+        }
+        if (to_string(res.size()).size() != digit) return {};
+        
+        part = 1;
+        for (string& s : res) {
+            string suffix = "<" + to_string(part) + "/" + to_string(res.size()) + ">";
+            s += suffix;
+            part++;
+        }
+        return res;
+        
+    }
+};
