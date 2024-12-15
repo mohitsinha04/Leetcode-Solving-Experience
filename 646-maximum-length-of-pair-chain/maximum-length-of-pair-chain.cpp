006cf2f6bb4caeb5dbfbe7d79ca0d0edc1697eaf
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-12-15 11:58:24 +0530

    Time: 5 ms (76.44%) | Memory: 26.5 MB (65.16%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end());
-        vector<int> dp(pairs.size() + 1, 0);
-        dp[pairs.size() - 1] = 1;
+        auto cmp = [&] (const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        };
         
-        for (int i = pairs.size() - 2; i >= 0; i--) {
-            int exclude = dp[i+1];
-            int next = binarySearch(pairs, i);
-            int include = dp[next] + 1;
-            dp[i] = max(exclude, include);
-        }
-        return dp[0];
-    }
-    int binarySearch(vector<vector<int>>& pairs, int curr_index) {
-        int left = curr_index + 1, right = pairs.size() - 1;
-        int res = pairs.size();
+        sort(pairs.begin(), pairs.end(), cmp);
         
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (pairs[mid][0] > pairs[curr_index][1]) {
-                res = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int max_len = 1;
+        int prev = pairs[0][1];
+        
+        for (int i= 1; i < pairs.size(); i++) {
+            if (pairs[i][0] > prev) {
+                max_len++;
+                prev = pairs[i][1];
             }
         }
-        return res;
+        
+        return max_len;
+        
+        
     }
 };
