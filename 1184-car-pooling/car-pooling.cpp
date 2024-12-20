dc47925111059bced18bbfce4b6bac973843e3a9
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-12-20 23:13:33 +0530

    Time: 2 ms (49.30%) | Memory: 14.7 MB (18.56%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        
-        int n = 0;
-        
-        for (auto trip : trips) n = max(trip[2], n);
-        
-        vector<int> line(n + 1, 0);
-        
+        vector<int> line(1001, 0);
         for (auto trip : trips) {
             line[trip[1]] += trip[0];
             line[trip[2]] -= trip[0];
         }
-        
-        if (line[0] > capacity) return false;
-        
-        for (int i = 1; i <= n; i ++) {
-            line[i] += line[i-1];
-            
-            if (line[i] > capacity) return false;
+        int currPassenger = 0;
+        for (int i = 0; i < 1001; i++) {
+            currPassenger += line[i];
+            if (currPassenger > capacity) return false;
         }
         
         return true;
