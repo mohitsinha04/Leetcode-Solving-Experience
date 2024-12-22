c98b30f893c20a91a2299c7e3381a70bcf67133d
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2024-12-22 23:57:06 +0530

    Time: 479 ms (39.89%) | Memory: 59.6 MB (71.09%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,55 +1,47 @@
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int source,
-                              int target) {
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        
         if (source == target) {
             return 0;
         }
-
-        unordered_map<int, vector<int>> adjList;
-        // Create a map from the bus stop to all the routes that include this
-        // stop.
-        for (int route = 0; route < routes.size(); route++) {
-            for (auto stop : routes[route]) {
-                // Add all the routes that have this stop.
-                adjList[stop].push_back(route);
+        
+        unordered_map<int, vector<int>> graph;
+        
+        for (int i = 0; i < routes.size(); i++) {
+            for (int stop : routes[i]) {
+                graph[stop].push_back(i);
             }
         }
-
+        
         queue<int> q;
         unordered_set<int> vis;
-        // Insert all the routes in the queue that have the source stop.
-        for (auto route : adjList[source]) {
-            q.push(route);
-            vis.insert(route);
+        
+        for (int r : graph[source]) {
+            q.push(r);
+            vis.insert(r);
         }
-
-        int busCount = 1;
-        while (q.size()) {
+        
+        int level = 1;
+        
+        while (!q.empty()) {
             int size = q.size();
-
             for (int i = 0; i < size; i++) {
-                int route = q.front();
+                int curr = q.front();
                 q.pop();
-
-                // Iterate over the stops in the current route.
-                for (auto stop : routes[route]) {
-                    // Return the current count if the target is found.
-                    if (stop == target) {
-                        return busCount;
-                    }
-
-                    // Iterate over the next possible routes from the current
-                    // stop.
-                    for (auto nextRoute : adjList[stop]) {
-                        if (!vis.count(nextRoute)) {
-                            vis.insert(nextRoute);
-                            q.push(nextRoute);
-                        }
-                    }
+                
+                for (int stop : routes[curr]) {
+                    if (stop == target) return level;
+                    
+                    for (int r : graph[stop]) {
+                        if (!vis.count(r)) {
+                            q.push(r);
+                            vis.insert(r);
+                        } 
+                    }  
                 }
             }
-            busCount++;
+            level++;
         }
         return -1;
     }
