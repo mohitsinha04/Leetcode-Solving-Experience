b9589a05b304ed6dda794ff717607b7f69e46578
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-06 23:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (86.81%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    int countComponents(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> graph;
-
-        for (auto edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+    int find(vector<int> &representative, int vertex) {
+        if (vertex == representative[vertex]) {
+            return vertex;
         }
-
-        vector<bool> visited(n, false);
-        int res = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(graph, i, visited);
-                res++;
+        
+        return representative[vertex] = find(representative, representative[vertex]);
+    }
+    
+    int combine(vector<int> &representative, vector<int> &size, int vertex1, int vertex2) {
+        vertex1 = find(representative, vertex1);
+        vertex2 = find(representative, vertex2);
+        
+        if (vertex1 == vertex2) {
+            return 0;
+        } else {
+            
+            if (size[vertex1] > size[vertex2]) {
+                size[vertex1] += size[vertex2];
+                representative[vertex2] = vertex1;
+            } else {
+                size[vertex2] += size[vertex1];
+                representative[vertex1] = vertex2;
             }
+            return 1;
         }
-        return res;
     }
 
-    void dfs(unordered_map<int, vector<int>>& graph, int source, vector<bool>& vis) {
-        vis[source] = true;
-        for (int i : graph[source]) {
-            if (vis[i]) continue;
-            dfs(graph, i, vis);
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> representative(n), size(n);
+        
+        for (int i = 0; i < n; i++) {
+            representative[i] = i;
+            size[i] = 1;
         }
+        
+        int components = n;
+        for (int i = 0; i < edges.size(); i++) {
+            components -= combine(representative, size, edges[i][0], edges[i][1]);
+        }
+
+        return components;
     }
 };
