79b170ba76c47e6dc88590fadd786d3309303ef5
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-07 16:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (76.35%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int lastDay = days[days.size() - 1];
-        vector<int> dp(lastDay + 1, -1);
-
-        unordered_set<int> days_set(days.begin(), days.end());
-
-        return solve(days_set, days, costs, dp, 1);
-    }
-
-    int solve (unordered_set<int> days_set, vector<int>& days, vector<int>& costs, vector<int>& dp, int currDay) {
-        if (currDay > days[days.size()-1]) 
-            return 0;
-        if (!days_set.count(currDay)) {
-            return solve(days_set, days, costs, dp, currDay + 1);
+        vector<int> dp(lastDay + 1, 0);
+        
+        int i = 0;
+        for (int day = 1; day <= lastDay; day++) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                i++;
+                dp[day] = min({dp[day - 1] + costs[0],
+                               dp[max(0, day - 7)] + costs[1],
+                               dp[max(0, day - 30)] + costs[2]});
+            }
         }
-        if (dp[currDay] != -1) return dp[currDay];
-
-        int day1 = costs[0] + solve(days_set, days, costs, dp, currDay + 1);
-        int day7 = costs[1] + solve(days_set, days, costs, dp, currDay + 7);
-        int day30 = costs[2] + solve(days_set, days, costs, dp, currDay + 30);
-
-        return dp[currDay] = min(day1, min(day7, day30));
-
+     
+        return dp[lastDay];
     }
 };
