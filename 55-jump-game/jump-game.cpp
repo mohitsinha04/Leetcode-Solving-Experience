a12b5742dd102e4aef96efc2704fcc01c313f16d
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-08 23:45:15 +0530

    Time: 95 ms (18.35%) | Memory: 54.8 MB (7.29%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -8,7 +8,7 @@ public:
             for (int j = i + 1; j <= farthest; j++) {
                 if (dp[j] ==1) {
                     dp[i] = 1;
-                    // break;
+                    break;
                 }
             }
         }
