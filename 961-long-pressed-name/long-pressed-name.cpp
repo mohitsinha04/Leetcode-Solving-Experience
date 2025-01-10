18c012aed059176d4b43fb76623dd965b6f89fc9
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-11 01:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (38.61%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0, j = 0;
+        if (name.size() > typed.size()) return false;
+        if (name.size() == typed.size()) return name == typed;
+        // if ()
+        // if (i )
+        while (i < name.size() && j < typed.size()) {
+            if (name[i] == typed[j]) {
+                i++;
+                j++;
+            } else if (j >= 1 && typed[j] == typed[j-1]) j++;
+            else return false;
+        }
+        // if (i == name.size() - 1 && j >= typed.size()) return name[i] == typed[j];
+        if (i != name.size()) return false;
+        while (j < typed.size()) {
+            if (typed[j] != typed[j-1]) return false;
+            j++;
+        }
+        return true;
+    }
+};
