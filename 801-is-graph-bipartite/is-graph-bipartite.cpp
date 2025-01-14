c492d5a815cc4b53c0a37e78137da064326fbe24
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-14 23:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (35.85%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> color(graph.size(), -1);
-
-        for (int i = 0; i < graph.size(); i++) {
-            if (color[i] == -1) {
-                if (dfs(graph, color, i, 0) == false) return false;
-            }
-        }
-        return true;
-    }
-
-    bool dfs(vector<vector<int>>& graph, vector<int>& color, int index, int currColor) {
-        color[index] = currColor;
-
-        for (auto i : graph[index]) {
-            if (color[i] == -1) {
-                if (!dfs(graph, color, i, !currColor)) return false;
-            } else {
-                if (color[i] == currColor) return false;
+        int n = graph.size();
+        vector<int> colors(n, 0);
+        queue<int> q;
+        
+        for (int i = 0; i < n; i++) {
+            if (colors[i]) continue;
+            
+            colors[i] = 1;
+            q.push(i);
+            
+            while (!q.empty()) {
+                int temp = q.front();
+                
+                for (auto neighbor : graph[temp]) {
+                    
+					// Color neighbor with opposite color
+                    if (!colors[neighbor]){
+                        colors[neighbor] = -colors[temp];
+                        q.push(neighbor);
+                    }
+                    
+					// If the neighbor has the same color - can't bipartite.
+                    else if (colors[neighbor] == colors[temp]) 
+                        return false;
+                }
+                q.pop();
             }
         }
         return true;
