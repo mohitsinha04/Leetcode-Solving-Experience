d93798547e7efe6e3bf9b1b5123cd87045fcfe3c
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-15 23:12:10 +0530

    Time: 195 ms (18.92%) | Memory: 71.2 MB (10.81%) - LeetSync

--- 2570-maximize-total-tastiness-of-purchased-fruits/maximize-total-tastiness-of-purchased-fruits.cpp
+++ 2570-maximize-total-tastiness-of-purchased-fruits/maximize-total-tastiness-of-purchased-fruits.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-
-    int maxTastiness(vector<int>& price, vector<int>& tastiness, int maxAmount, int maxCoupons) {
-        vector<vector<vector<int>>> dp(price.size(), vector<vector<int>> (maxAmount + 1, vector<int> (maxCoupons + 1, -1)));
-        return dfs(price, tastiness, 0, maxAmount, maxCoupons, dp);
-    }
-
-    int dfs(vector<int>& price, vector<int>& tastiness, int idx, int maxAmount, int maxCoupons, vector<vector<vector<int>>> &dp){
-        if(idx == price.size()) return 0;
-        if(dp[idx][maxAmount][maxCoupons] != -1) return dp[idx][maxAmount][maxCoupons];
-
-        int notTake = dfs(price, tastiness, idx + 1, maxAmount, maxCoupons, dp);
+    int maxTastiness(vector<int>& price, vector<int>& tastiness, int barAmount, int couponMax) {
+        vector<vector<vector<int>>> dp(price.size()+1, vector<vector<int>> (barAmount + 1, vector<int> (couponMax + 1, 0)));
+        int n = price.size();
         
-        int take = 0, takeCoupon = 0;
-        if(price[idx] <= maxAmount){
-            take = tastiness[idx] + dfs(price, tastiness, idx + 1, maxAmount - price[idx], maxCoupons, dp);
+        for(int idx = n-1;idx>=0;idx--){
+            for(int maxAmount = barAmount; maxAmount >=0; maxAmount--){
+                for(int maxCoupons = couponMax; maxCoupons >=0; maxCoupons--){
+                        
+                        int notTake = dp[idx + 1][maxAmount][maxCoupons];
+                        int take = 0, takeCoupon = 0;
+                        if(price[idx] <= maxAmount){
+                            take = tastiness[idx] + dp[idx + 1][maxAmount - price[idx]][maxCoupons];
+                        }
+                        if(maxCoupons > 0 && price[idx]/2 <= maxAmount){
+                            takeCoupon = tastiness[idx] + dp[idx + 1][maxAmount - price[idx]/2][maxCoupons-1];
+                        }
+                        dp[idx][maxAmount][maxCoupons] = max({take, takeCoupon, notTake});
+                }
+            }
         }
-        if(maxCoupons > 0 && price[idx]/2 <= maxAmount){
-            takeCoupon = tastiness[idx] + dfs(price, tastiness, idx + 1, maxAmount - price[idx]/2, maxCoupons-1, dp);
-        }
-        return dp[idx][maxAmount][maxCoupons] = max({take, takeCoupon, notTake});
+        return dp[0][barAmount][couponMax];
     }
 };
