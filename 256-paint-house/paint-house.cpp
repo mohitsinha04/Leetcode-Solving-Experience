6c059138a53c478895faa6fedc25e17f56a3db1e
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-15 20:16:49 +0530

    Time: 3 ms (22.98%) | Memory: 13.3 MB (60.29%) - LeetSync

--- 256-paint-house/paint-house.cpp
+++ 256-paint-house/paint-house.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
+        if (costs.size() == 0) return 0;
+        vector<int> prev = costs[costs.size() - 1];
 
         for (int i = costs.size() - 2; i >= 0; i--) {
-            costs[i][0] += min(costs[i + 1][1], costs[i + 1][2]);
-            costs[i][1] += min(costs[i + 1][0], costs[i + 1][2]);
-            costs[i][2] += min(costs[i + 1][0], costs[i + 1][1]);
+            vector<int> curr = costs[i];
+            curr[0] += min(prev[1], prev[2]);
+            curr[1] += min(prev[0], prev[2]);
+            curr[2] += min(prev[1], prev[0]);
+            prev = curr;
         }
-
-        if (costs.size() == 0) return 0;
-
-        return min(costs[0][0], min(costs[0][1], costs[0][2]));
-        
+        return min(prev[0], min(prev[1], prev[2]));
     }
 };
