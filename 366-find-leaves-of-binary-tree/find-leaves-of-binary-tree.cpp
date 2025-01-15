1eca90be84d98b2c5e7eb94b079fe0ff7d5fdca6
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-16 00:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.18%) - LeetSync

--- 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.cpp
@@ -11,49 +11,25 @@
  */
 class Solution {
 public:
+    vector<vector<int>> res;
     vector<vector<int>> findLeaves(TreeNode* root) {
-        unordered_map<TreeNode*, int> inDegree; // child is prerequisite in this problem
-        unordered_map<TreeNode*, vector<TreeNode*>> graph; //child -> parent
-        stack<TreeNode*> stk;
-        stk.push(root);
-        while(!stk.empty()) {
-            TreeNode* curr = stk.top();
-            stk.pop();
-            int child = 0;
-            if(curr->left) {
-                stk.push(curr->left);
-                graph[curr->left].push_back(curr); // the prerequisite of parent is its children
-                ++child;
-            }
-            if(curr->right) {
-                stk.push(curr->right);
-                graph[curr->right].push_back(curr);
-                ++child;
-            }
-            inDegree[curr] = child;
+        helper(root);
+        return res;
+    }
+    int helper(TreeNode* root) {
+        if (root == nullptr) {
+            return -1;
         }
+        int left = helper(root->left);
+        int right = helper(root->right);
 
-        // normal topological sort code
-        queue<TreeNode*> bfs;
-        for(auto& [node, child] : inDegree) {
-            if(child == 0)
-                bfs.push(node);
+        int currHeight = max(left, right) + 1;
+
+        if (currHeight == res.size()) {
+            vector<int> curr;
+            res.push_back(curr);
         }
-        vector<vector<int>> ans;
-        while(!bfs.empty()) {
-            ans.push_back({});
-            int sz = bfs.size();
-            for(int i=0; i<sz; i++) {
-                auto node = bfs.front();
-                ans.back().push_back(node->val);
-                bfs.pop();
-                for(auto neigh : graph[node]) {
-                    inDegree[neigh] -= 1;
-                    if(inDegree[neigh] == 0)
-                        bfs.push(neigh);
-                }
-            }
-        }
-        return ans;
+        res[currHeight].push_back(root->val);
+        return currHeight;
     }
 };
