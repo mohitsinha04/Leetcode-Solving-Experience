16a070940808114a59d126411de9a41be59479ed
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-18 14:49:31 +0530

    Time: 3 ms (39.15%) | Memory: 17 MB (55.63%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int position = 0;
-        while (tokens.size() > 1) {
-            while (tokens[position] != "+" && tokens[position] != "-" &&
-                   tokens[position] != "*" && tokens[position] != "/") {
-                position++;
+        stack<int> stack;
+        string operators = "+-*/";
+        for (string token : tokens) {
+            if (operators.find(token) == string::npos) {
+                stack.push(stoi(token));
+                continue;
             }
-            int number1 = std::stoi(tokens[position - 2]);
-            int number2 = std::stoi(tokens[position - 1]);
-            int newValue = 0;
-            if (tokens[position] == "+") {
-                newValue = number1 + number2;
-            } else if (tokens[position] == "-") {
-                newValue = number1 - number2;
-            } else if (tokens[position] == "*") {
-                newValue = number1 * number2;
-            } else if (tokens[position] == "/") {
-                newValue = number1 / number2;
+            int number2 = stack.top();
+            stack.pop();
+            int number1 = stack.top();
+            stack.pop();
+            int result = 0;
+            if (token == "+") {
+                result = number1 + number2;
+            } else if (token == "-") {
+                result = number1 - number2;
+            } else if (token == "*") {
+                result = number1 * number2;
+            } else if (token == "/") {
+                result = number1 / number2;
             }
-            tokens[position] = std::to_string(newValue);
-            tokens.erase(tokens.begin() + position - 2,
-                         tokens.begin() + position);
-            position--;
+            stack.push(result);
         }
-        return std::stoi(tokens[0]);
+        return stack.top();
     }
 };
