7c14a5faafbef63e4c4f0d58f4751680e22f8dec
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-18 14:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (37.72%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,39 +1,35 @@
 class MinStack {
 public:
-
-    std::stack<int> stack;
-    std::stack<pair<int, int>> minStack;
-
+    int min = INT_MAX;
+    stack<int> stack;
     MinStack() {
         
     }
     
     void push(int val) {
-        stack.push(val);
-        if (minStack.empty() || val < minStack.top().first) minStack.push({val, 1});
-
-        else if (minStack.top().first == val) {
-            minStack.top().second++;
+        if (val <= min) {
+            stack.push(min);
+            min = val;
         }
+        stack.push(val);
     }
     
     void pop() {
         int curr = stack.top();
         stack.pop();
-
-        if (minStack.top().first == curr) {
-            if (minStack.top().second > 1) minStack.top().second--;
-            else minStack.pop();
+        if (curr == min) {
+            min = stack.top();
+            stack.pop();
         }
+        
     }
     
     int top() {
         return stack.top();
-        
     }
     
     int getMin() {
-        return minStack.top().first;
+        return min;
     }
 };
 
