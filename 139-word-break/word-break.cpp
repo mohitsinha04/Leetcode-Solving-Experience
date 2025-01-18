9fccf11c70b069113d8be7a2401cde692ee22121
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-01-18 13:21:26 +0530

    Time: 2 ms (75.02%) | Memory: 11.8 MB (70.77%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
+    vector<int> memo;
+    vector<string> wordDict;
+    string s;
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        queue<int> q;
-        vector<bool> seen(s.size(), false);
-        q.push(0);
+        memo = vector(s.length(), -1);
+        this->wordDict = wordDict;
+        this->s = s;
+        return dp(s.length() - 1);
+    }
 
-        while (!q.empty()) {
-            int start = q.front();
-            q.pop();
+    bool dp(int i) {
+        if (i < 0) return true;
 
-            if (start == s.size()) return true;
+        if (memo[i] != -1) {
+            return memo[i] == 1;
+        }
 
-            for (int end = start + 1; end <= s.size(); end++) {
-                if (seen[end]) continue;
-
-                if (words.count(s.substr(start, end - start))) {
-                    q.push(end);
-                    seen[end] = true;
-                }
+        for (string word : wordDict) {
+            int currSize = word.length();
+            // Handle out of bounds case
+            if (i - currSize + 1 < 0) {
+                continue;
+            }
+            string currword = s.substr(i - currSize + 1, currSize);
+            if (s.substr(i - currSize + 1, currSize) == word &&
+                dp(i - currSize)) {
+                memo[i] = 1;
+                return true;
             }
         }
+
+        memo[i] = 0;
         return false;
     }
 };
