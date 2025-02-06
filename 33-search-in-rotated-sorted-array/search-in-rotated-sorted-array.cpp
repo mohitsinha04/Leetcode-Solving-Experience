842cfac906648dda011998f984042ef1523a4a20
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-02-06 15:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.31%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-
+        int n = nums.size();
+        int left = 0, right = n - 1;
         while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] > nums[nums.size() - 1]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            int mid = left + (right - left) / 2;
+            // Case 1: Find target
+            if (nums[mid] == target) {
+                return mid;
             }
-        }
-        int start = left;
-        if (nums[start] <= target && nums[nums.size() - 1] >= target) {
-            left = start;
-            right = nums.size() - 1;
-        } else {
-            left = 0;
-            right = start - 1;
-        }
-
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
+            // Case 2: Subarray on mid's left is sorted
+            else if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Case 3: Subarray on mid's right is sorted
+            else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
         return -1;
     }
