f581fef3c4c4bd705ab3a0c2b64a945775d7efdc
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-11 23:25:40 +0530

    Time: 401 ms (10.33%) | Memory: 7.9 MB (95.02%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(0,s,0,p);    
+    }
+    bool isMatch(int i, string& s, int j, string &p) {
+        int pn=p.size(), sn = s.size();
+        if(j==pn) return i==sn;
+        if(p[j+1]=='*') {
+            if(isMatch(i,s,j+2,p) || 
+               i<sn && (p[j] == '.' || s[i] == p[j]) && isMatch(i+1,s,j,p)) 
+			   return 1;
+        } else if (i<sn && (p[j]=='.'|| s[i]==p[j]) && isMatch(i+1,s,j+1,p)) 
+			return 1;
+        return 0;
+    }
+};
