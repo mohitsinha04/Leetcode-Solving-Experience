c14ec7ecafa8c886d2fdd855fb8bd900fb4b6df8
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-11 22:46:31 +0530

    Time: 31 ms (62.95%) | Memory: 14.8 MB (61.24%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -27,11 +27,44 @@ public:
         return dp[i][j]=0;
     }
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        // int n = s.size();
+        // int m = p.size();
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
 
-        return helper(n,m,s,p,dp);
+        // return helper(n,m,s,p,dp);
 
+        int n=s.length(),m=p.length();
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1));
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++) dp[i][0]=false;
+        
+        bool flag=true;
+        for(int i=1;i<=m;i++)
+        {
+            if(p[i-1]!='*')
+                flag=false;
+            dp[0][i]=flag;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='*')
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
+
+    }
+    bool isAllStars(string &S1, int i) {
+        // S1 is taken in 1-based indexing
+        for (int j = 1; j <= i; j++) {
+            if (S1[j - 1] != '*')
+                return false;
+        }
+        return true;
     }
 };
