203a3ed555e8bae272237b05a5c487bb5c30f128
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-12 23:04:07 +0530

    Time: 11 ms (9.94%) | Memory: 18.3 MB (8.19%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.cpp
+++ 252-meeting-rooms/meeting-rooms.cpp
@@ -5,18 +5,35 @@ public:
     // [[0,30],[60,240],[90,120]]
     
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        if (intervals.size() <= 1) return true;
-        auto cmp = [&] (const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        };
+        // if (intervals.size() <= 1) return true;
+        // auto cmp = [&] (const vector<int>& a, vector<int>& b) {
+        //     return a[1] < b[1];
+        // };
         
-        sort(intervals.begin(), intervals.end(), cmp);
-        int prev = intervals[0][1];
+        // sort(intervals.begin(), intervals.end(), cmp);
+        // int prev = intervals[0][1];
         
-        for (int i = 1; i < intervals.size(); i++) {
-            if (prev > intervals[i][0]) return false;
-            prev = intervals[i][1];
+        // for (int i = 1; i < intervals.size(); i++) {
+        //     if (prev > intervals[i][0]) return false;
+        //     prev = intervals[i][1];
+        // }
+        // return true;
+        map<int, int> line;
+        
+        for (auto interval : intervals) {
+            line[interval[0]]++;
+            line[interval[1]]--;
         }
-        return true;
+        
+        int res = 0;
+        int count = 0;
+        
+        for (auto l : line) {
+            count += l.second;
+            if (count > 1) return false;
+            res = max(res, count);
+        }
+        
+        return res <= 1 ? true : false;
     }
 };
