745907a5526b67eeac89c24f560521cdb010a86a
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-16 21:18:35 +0530

    Time: 19 ms (34.96%) | Memory: 26.7 MB (83.39%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -12,43 +12,55 @@
 class Solution {
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        if (!root) {
+            return {};
+        }
+
         unordered_set<int> toDeleteSet(to_delete.begin(), to_delete.end());
         vector<TreeNode*> forest;
 
-        root = processNode(root, toDeleteSet, forest);
+        queue<TreeNode*> nodesQueue;
+        nodesQueue.push(root);
 
-        // If the root is not deleted, add it to the forest
-        if (root) {
+        while (!nodesQueue.empty()) {
+            TreeNode* currentNode = nodesQueue.front();
+            nodesQueue.pop();
+
+            if (currentNode->left) {
+                nodesQueue.push(currentNode->left);
+                // Disconnect the left child if it needs to be deleted
+                if (toDeleteSet.find(currentNode->left->val) !=
+                    toDeleteSet.end()) {
+                    currentNode->left = nullptr;
+                }
+            }
+
+            if (currentNode->right) {
+                nodesQueue.push(currentNode->right);
+                // Disconnect the right child if it needs to be deleted
+                if (toDeleteSet.find(currentNode->right->val) !=
+                    toDeleteSet.end()) {
+                    currentNode->right = nullptr;
+                }
+            }
+
+            // If the current node needs to be deleted, add its non-null
+            // children to the forest
+            if (toDeleteSet.find(currentNode->val) != toDeleteSet.end()) {
+                if (currentNode->left) {
+                    forest.push_back(currentNode->left);
+                }
+                if (currentNode->right) {
+                    forest.push_back(currentNode->right);
+                }
+            }
+        }
+
+        // Ensure the root is added to the forest if it is not to be deleted
+        if (toDeleteSet.find(root->val) == toDeleteSet.end()) {
             forest.push_back(root);
         }
 
         return forest;
-    }
-
-private:
-    TreeNode* processNode(TreeNode* node, unordered_set<int>& toDeleteSet,
-                          vector<TreeNode*>& forest) {
-        if (!node) {
-            return nullptr;
-        }
-
-        node->left = processNode(node->left, toDeleteSet, forest);
-        node->right = processNode(node->right, toDeleteSet, forest);
-
-        // Node Evaluation: Check if the current node needs to be deleted
-        if (toDeleteSet.find(node->val) != toDeleteSet.end()) {
-            // If the node has left or right children, add them to the forest
-            if (node->left) {
-                forest.push_back(node->left);
-            }
-            if (node->right) {
-                forest.push_back(node->right);
-            }
-            // Delete the current node and return null to its parent
-            delete node;
-            return nullptr;
-        }
-
-        return node;
     }
 };
