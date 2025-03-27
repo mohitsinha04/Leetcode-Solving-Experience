f9ef115597e11b20916deb86067812f9dbaa7665
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-27 21:02:08 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (92.77%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        int boom = 0;
+        traverseDFS(root->left, root->right, boom);
+        return root;
+    }
+    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+        if (leftChild == nullptr || rightChild == nullptr) {
+            return;
+        }
+        // If the current level is odd, swap the values of the children.
+        if (level % 2 == 0) {
+            int temp = leftChild->val;
+            leftChild->val = rightChild->val;
+            rightChild->val = temp;
+        }
+
+        traverseDFS(leftChild->left, rightChild->right, level + 1);
+        traverseDFS(leftChild->right, rightChild->left, level + 1);
+    }
+};
