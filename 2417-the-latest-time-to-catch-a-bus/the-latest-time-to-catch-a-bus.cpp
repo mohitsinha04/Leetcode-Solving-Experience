1cf88f1a684c74cfc125a0b7480ba79d394c6a29
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-30 14:18:38 +0530

    Time: 89 ms (37.26%) | Memory: 97.7 MB (35.38%) - LeetSync

---
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int latestTimeCatchTheBus(vector<int>& b, vector<int>& p, int cap) {
+        int n1 = b.size() , n2 = p.size() , j=0 , ans;
+        unordered_set<int> u;
+        sort(b.begin() , b.end());
+        sort(p.begin() , p.end());
+        
+        for(int i=0 ; i<n1 ; i++){
+            int c = 0;
+            while(c<cap && j<n2 && p[j]<=b[i]){
+                if(u.find(p[j]-1)==u.end()) ans = p[j]-1; 
+                u.insert(p[j]);
+                c++;
+                j++;
+            }
+            if(c<cap && u.find(b[i])==u.end()) ans = b[i];
+        }
+        return ans;
+    }
+};
