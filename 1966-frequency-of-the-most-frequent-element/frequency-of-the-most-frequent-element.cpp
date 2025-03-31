49fd6dc07e48f9966762ada22dcb6bff1c5ec076
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-03-31 15:29:06 +0530

    Time: 99 ms (7.16%) | Memory: 127.8 MB (5.47%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,21 +1,65 @@
+// class Solution {
+// public:
+//     int maxFrequency(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         int left = 0;
+//         int ans = 0;
+//         long curr = 0;
+        
+//         for (int right = 0; right < nums.size(); right++) {
+//             long target = nums[right];
+//             curr += target;
+            
+//             while ((right - left + 1) * target - curr > k) {
+//                 curr -= nums[left];
+//                 left++;
+//             }
+            
+//             ans = max(ans, right - left + 1);
+//         }
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
+    int check(int i, int k, vector<int>& nums, vector<long>& prefix) {
+        int target = nums[i];
+        int left = 0;
+        int right = i;
+        int best = i;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long count = i - mid + 1;
+            long finalSum = count * target;
+            int originalSum = prefix[i] - prefix[mid] + nums[mid];
+            int operationsRequired = finalSum - originalSum;
+            
+            if (operationsRequired > k) {
+                left = mid + 1;
+            } else {
+                best = mid;
+                right = mid - 1;
+            }
+        }
+        
+        return i - best + 1;
+    }
+    
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int left = 0;
-        int ans = 0;
-        long curr = 0;
+        vector<long> prefix;
+        prefix.push_back(nums[0]);
         
-        for (int right = 0; right < nums.size(); right++) {
-            long target = nums[right];
-            curr += target;
-            
-            while ((right - left + 1) * target - curr > k) {
-                curr -= nums[left];
-                left++;
-            }
-            
-            ans = max(ans, right - left + 1);
+        for (int i = 1; i < nums.size(); i++) {
+            prefix.push_back(nums[i] + prefix.back());
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ans = max(ans, check(i, k, nums, prefix));
         }
         
         return ans;
