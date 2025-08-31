6d745b54c8390b1d4fc8957f11e82e1d907be876
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-08-31 14:25:50 +0530

    Time: 11 ms (57.66%) | Memory: 28.7 MB (5.11%) - LeetSync

--- 244-shortest-word-distance-ii/shortest-word-distance-ii.cpp
+++ 244-shortest-word-distance-ii/shortest-word-distance-ii.cpp
@@ -1,24 +1,28 @@
 class WordDistance {
 public:
-    unordered_map<string, vector<int>> wordMap;
-
+    vector<string> wordsDict;
+    unordered_map<string, vector<int>> wordsIndex;
     WordDistance(vector<string>& wordsDict) {
+        this->wordsDict = wordsDict;
+
         for (int i = 0; i < wordsDict.size(); i++) {
-            string s = wordsDict[i];
-            wordMap[s].push_back(i);
+            wordsIndex[wordsDict[i]].push_back(i);
         }
     }
     
     int shortest(string word1, string word2) {
-        vector<int> word1Array = wordMap[word1];
-        vector<int> word2Array = wordMap[word2];
+        vector<int> word1Indices = wordsIndex[word1];
+        vector<int> word2Indices = wordsIndex[word2];
 
-        int i =0, j = 0, res = INT_MAX;
-        while (i < word1Array.size() && j < word2Array.size()) {
-            res = min(res, abs(word1Array[i] - word2Array[j]));
+        int i = 0, j = 0;
+        int res = wordsDict.size();
+        while (i < word1Indices.size() && j < word2Indices.size()) {
+            int curr = abs(word1Indices[i] - word2Indices[j]);
+            res = min(res, curr);
 
-            if (word1Array[i] < word2Array[j]) i++;
-            else j++;
+            if (word1Indices[i] < word2Indices[j]) {
+                i++;
+            } else j++;
         }
         return res;
     }
@@ -29,3 +33,10 @@ public:
  * WordDistance* obj = new WordDistance(wordsDict);
  * int param_1 = obj->shortest(word1,word2);
  */
+
+
+
+// practice -> 0
+// makes -> 1, 4
+// perfect = 2
+// coding = 3
