9ca5f04a117cd5f10a993ff11c0caab3b715594f
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-08-31 14:12:56 +0530

    Time: 2 ms (20.43%) | Memory: 15.7 MB (21.34%) - LeetSync

--- 243-shortest-word-distance/shortest-word-distance.cpp
+++ 243-shortest-word-distance/shortest-word-distance.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
-        int lastword1 = -1;
+        int lastWord1 = -1;
         int lastword2 = -1;
+
         int res = wordsDict.size();
         for (int i = 0; i < wordsDict.size(); i++) {
             if (wordsDict[i] == word1) {
-                lastword1 = i;
+                lastWord1 = i;
             } else if (wordsDict[i] == word2) {
                 lastword2 = i;
-            } 
-            if (lastword1 != -1 && lastword2 != -1) {
-                int currDist = abs(lastword1 - lastword2);
-                res = min(res, currDist);
+            }
+            if (lastWord1 != -1 && lastword2 != -1) {
+                int currLen = abs(lastWord1 - lastword2);
+                res = min(currLen, res);
             }
         }
         return res;
     }
 };
+
+// practice -> 0
+// makes -> 1, 4
+// perfect = 2
+// coding = 3
