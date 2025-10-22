f6e74d898b69d46bd9a2098941e39c1a616e35ae
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-10-22 23:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (7.98%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
         vector<int> population_year(2051, 0);
+        int earliestYear = INT_MAX, oldestYear = INT_MIN;
         for (auto i : logs) {
             population_year[i[0]]++;
             population_year[i[1]]--;
+            earliestYear = min(i[0], earliestYear);
+            oldestYear = max(i[1], oldestYear);
         }
         
         int maxPopulation = 0;
         int maxPopIndex = -1;
         
-        for (int i = 1950; i <= 2050; i++) {
+        for (int i = earliestYear; i <= oldestYear; i++) {
             population_year[i] += population_year[i-1];
             
             if (population_year[i] > maxPopulation) {
