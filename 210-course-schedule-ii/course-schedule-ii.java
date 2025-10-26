db80a007144de5acdb5edf608ca422641387c447
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-10-26 12:47:31 +0530

    Time: 7 ms (27.48%) | Memory: 46.1 MB (52.40%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -3,43 +3,33 @@ class Solution {
         int[] indegree = new int[numCourses];
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
 
-        for (int[] prerequisite : prerequisites) {
-            int first = prerequisite[0];
-            int sec = prerequisite[1];
+        for (int[] p : prerequisites) {
+            int first = p[0];
+            int sec = p[1];
             indegree[first]++;
             graph.computeIfAbsent(sec, k -> new ArrayList<>()).add(first);
         }
-
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < indegree.length; i++) {
+        for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) q.offer(i);
         }
 
         ArrayList<Integer> res = new ArrayList<>();
-
         while (!q.isEmpty()) {
             int curr = q.poll();
-            res.add(curr);
 
-            for (int next : graph.getOrDefault(curr, new ArrayList<>())) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
+            res.add(curr);
+            for (int course : graph.getOrDefault(curr, new ArrayList<>())) {
+                indegree[course]--;
+                if (indegree[course] == 0) q.offer(course);
             }
         }
 
-        int[] intArray = new int[res.size()];
-
-        // 3. Loop and copy (unboxing occurs automatically)
-        for (int i = 0; i < res.size(); i++) {
-            // Automatic unboxing converts Integer to int
-            intArray[i] = res.get(i); 
+        if (res.size() != numCourses) return new int[0];
+        int[] results = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            results[i] = res.get(i);
         }
-        if (res.size() != numCourses) {
-            return new int[0];
-        }   
-        return intArray;
-
+        return results;
     }
 }
