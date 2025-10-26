07c13845bd2f521ac6170f3849944bc7f648298f
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2025-10-26 11:42:51 +0530

    Time: 19 ms (11.40%) | Memory: 17 MB (9.75%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -2,58 +2,31 @@ class Solution {
 public:
     int calculate(string s) {
         int i = 0;
-        return parseExpressionstack(s, i);
+        return parseExpr(s, i);
     }
-    int parseExpression(string& s, int& i) {
+    int parseExpr(string& s, int& i) {
         //this vector is working like stack here!
         vector<int> nums;
-        char op = '+';
-        while (i < s.size() && op != ')') {
+        char operation = '+';
+        while (i < s.size() && operation != ')') {
             if (s[i] == ' ') {
                 i++;
                 continue;
             }
-            long n = s[i] == '(' ? parseExpression(s, ++i) : parseNum(s, i);
-            switch(op) {
+            // check bracket and number
+
+            long n = s[i] == '(' ? parseExpr(s, ++i) : parseNum(s, i);
+            switch(operation) {
                 case '+' : nums.push_back(n); break;
                 case '-' : nums.push_back(-n); break;
                 // case '*' : nums.back() *= n; break;
                 // case '/' : nums.back() /= n; break;
             }            
             if (i < s.length())                 
-                op = s[i];
+                operation = s[i];
             i++;
         }
         return accumulate(nums.begin(), nums.end(), 0);
-    }
-
-    int parseExpressionstack(string& s, int& i) {
-        //this vector is working like stack here!
-        stack<int> nums;
-        char op = '+';
-        while (i < s.size() && op != ')') {
-            if (s[i] == ' ') {
-                i++;
-                continue;
-            }
-            long n = s[i] == '(' ? parseExpression(s, ++i) : parseNum(s, i);
-            switch(op) {
-                case '+' : nums.push(n); break;
-                case '-' : nums.push(-n); break;
-                // case '*' : nums.back() *= n; break;
-                // case '/' : nums.back() /= n; break;
-            }            
-            if (i < s.length())                 
-                op = s[i];
-            i++;
-        }
-        int res = 0;
-        while (!nums.empty()) {
-            res += nums.top();
-            nums.pop();
-        }
-        return res;
-        // return accumulate(nums.begin(), nums.end(), 0);
     }
     
     long parseNum(string& s, int& i) {           
