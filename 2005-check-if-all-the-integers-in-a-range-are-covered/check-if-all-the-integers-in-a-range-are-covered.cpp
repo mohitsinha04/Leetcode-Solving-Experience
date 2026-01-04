15c7d8479325d7744e1b9c6c3ace5f1bbd783b73
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-01-04 16:33:50 +0530

    Time: 3 ms (17.32%) | Memory: 12.7 MB (7.22%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        vector<int> line(100, 0);
-        
-        for (auto range : ranges) {
-            line[range[0]]++;
-            line[range[1] + 1]--;
+        unordered_map<int, int> line;
+        for (auto r : ranges) {
+            line[r[0]]++;
+            line[r[1] + 1]--;
         }
-        
         int count = 0;
         
         for (int i = 0; i < 55; i++) {
