3df224d34f9fff0d36443915f236712549df7bfa
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-01-08 23:05:30 +0530

    Time: 54 ms (32.29%) | Memory: 93.8 MB (73.68%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,13 +5,16 @@ public:
             
             return a[1] < b[1];  
         };
-        
+        // [[10,16],[2,8],[1,6],[7,12]]
+        // [[1,6],[2,8],[7,12],[10,16]]
         sort(points.begin(), points.end(), cmp);
         
         int prev = points[0][1];
+        // prev = 6;
         int minArrows = 1;
         
         for (int i = 1; i < points.size(); i++) {
+            // 7 < 10
             if (prev < points[i][0]) {
                 minArrows++;
                 prev = points[i][1];
