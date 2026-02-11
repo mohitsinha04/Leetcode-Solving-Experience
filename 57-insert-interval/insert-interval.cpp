eb4e1b1d637be69e697c5a8d6dc2f2ef759f9b99
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-02-11 23:37:20 +0530

    Time: 5 ms (10.50%) | Memory: 22 MB (12.67%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        map<int, int> line;
-
-        for (auto interval : intervals) {
-            line[interval[0]]++;
-            line[interval[1]]--;
-        }
-        line[newInterval[0]]++;
-        line[newInterval[1]]--;
-
-        int count = 0, start = 0;
         vector<vector<int>> res;
-        for (auto l : line) {
-            if (count == 0) {
-                start = l.first;
-            }
-            count += l.second;
-            if (count == 0) {
-                res.push_back({start, l.first});
+        for (auto interval : intervals) {
+            if (interval[1] < newInterval[0]) {
+                res.push_back(interval);
+            } else if (newInterval[1] < interval[0]) {
+                res.push_back(newInterval);
+                newInterval = interval;
+            } else if (interval[1] >= newInterval[0] || newInterval[1] <= interval[0] ) {
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);   
             }
         }
+        res.push_back(newInterval);
         return res;
     }
 };
