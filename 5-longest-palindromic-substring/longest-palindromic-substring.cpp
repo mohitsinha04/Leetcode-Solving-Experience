9a6cdd77772d6302c25f1a0bbb0cc45d4a14d03d
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-03-21 14:44:40 +0530

    Time: 88 ms (31.10%) | Memory: 174.1 MB (14.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    string expand(int i, int j, string s) {
-        int left = i;
-        int right = j;
 
+    string expand (int left, int right, string s) {
         while (left >= 0 && right < s.size() && s[left] == s[right]) {
             left--;
             right++;
         }
-
         return s.substr(left + 1, right - left - 1);
     }
-
     string longestPalindrome(string s) {
-        string ans = "";
+        string res = "";
 
         for (int i = 0; i < s.size(); i++) {
             string odd = expand(i, i, s);
-            if (odd.size() > ans.size()) {
-                ans = odd;
+            if (odd.size() > res.size()) {
+                res = odd;
             }
 
-            string even = expand(i, i + 1, s);
-            if (even.size() > ans.size()) {
-                ans = even;
+            string even = expand(i, i+1, s);
+            if (even.size() > res.size()) {
+                res = even;
             }
         }
-
-        return ans;
+        return res;
     }
 };
