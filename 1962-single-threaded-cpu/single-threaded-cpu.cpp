6b42df830143c1b7464603ce04d55c487f93082d
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-04-14 01:35:13 +0530

    Time: 147 ms (34.91%) | Memory: 133.8 MB (59.99%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int> temp;
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        int n = tasks.size();
+
+        // Preserve original indices
+        for(int i = 0; i < n; i++){
+            tasks[i].push_back(i);
+        }
+
+        sort(tasks.begin(), tasks.end());
+        
+        int i = 0;
+        unsigned long t = tasks[0][0];
+
+        while(!pq.empty() || i < n){
+            // Load all arrived tasks into the queue
+            while(i < n && t >= tasks[i][0]){
+                pq.push({tasks[i][1], tasks[i][2]});
+                i++;
+            }
+
+            // Process the shortest task
+            if(!pq.empty()){
+                t += pq.top().first;
+                temp.push_back(pq.top().second);
+                pq.pop();
+            } else {
+                // TLE Bypass: Teleport clock to the next task's arrival time
+                t = tasks[i][0];
+            }
+        }
+
+        return temp;
+    }
+};
