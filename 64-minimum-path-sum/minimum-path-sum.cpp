c61921c67b4c14a1e287a85733133b9aaef9ed6d
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-07-22 15:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (42.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        // dp[0][0] = grid[0][0];
-        return helper(grid, 0, 0, dp);
+        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), grid[0][0]));
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.size(); i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for (int j = 1; j < grid[0].size(); j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        for (int i = 1; i < grid.size(); i++) {
+            for (int j = 1; j < grid[0].size(); j++) {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[grid.size()-1][grid[0].size()-1];
     }
     int helper(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp) {
         if (dp[i][j] != -1) return dp[i][j];
