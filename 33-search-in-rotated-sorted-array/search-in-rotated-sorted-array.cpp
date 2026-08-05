32afb32cdf8b260d1ea174e3b5197ba215cf4fbf
Mohit Sinha <32459409+mohitsinha04@users.noreply.github.com>
2026-08-06 00:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int left = 0, right = n - 1;
+        int left = 0, right = nums.size() -1;
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        right = nums.size() - 1;
+        if (target >= nums[left] && target <= nums[right]) {
+            
+        } else {
+            right = left - 1;
+            left = 0;
+        }
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            // Case 1: Find target
+            int mid = left + (right - left) /2;
             if (nums[mid] == target) {
                 return mid;
             }
-            // Case 2: Subarray on mid's left is sorted
-            else if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            // Case 3: Subarray on mid's right is sorted
-            else {
-                if (target <= nums[right] && target > nums[mid]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return -1;
